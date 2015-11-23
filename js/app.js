196f2e0b375c58ebedb304ef6e43232b517dd5fa
anthonyz0777 <anthonyz0777@gmail.com>
2015-11-23 00:33:02 -0600

    New Update for View and form

--- js/app.js
+++ js/app.js
@@ -1,58 +1,64 @@
 var app = angular.module('myApp', []);
 
+
 app.config(function($httpProvider) {
-    $httpProvider.defaults.xsrfCookieName = 'csrftoken';
-    $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
-});
-
+                $httpProvider.defaults.xsrfCookieName = 'csrftoken';
+                $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
+            });
 app.controller('MyController', function ($scope, $http){
-            
-            $scope.hello = function(){
-                console.log("GET");
-            
 
-            //-------------GET--------------    
+    
+        $scope.hello = function(){
+                console.log("OPTIONS");
+            
                 
-              
-              
-            $http({method: 'OPTIONS', url: 'http://192.241.211.179:8000/rest-auth/registration/', headers:  {
-                        'Accept': 'application/json',
-                        'Content-Type': 'application/json',
-                        
-                        
-                    }}).
-                success(function successCallback(response) {
-                console.log(response);
+                //-------------GET--------------    
 
-                
-                
-                }).
-                error(function(data) {
-                 alert("fallo");
-                });
+
+                $http({method: 'GET', url: 'http://192.241.211.179:8000/users/', headers:  {
+                            'Accept': 'application/json',
+                            'Content-Type': 'application/json'
+
+                        }}).success(function successCallback(response) {
+                        console.log(response);
+                        }).
+                        error(function(data) {
+                         alert("fallo");
+                    });
+            
                 //-------------POST--------------
-                        
+                        if($scope.offers==undefined){
+                            $scope.offers=false;                            
+                        }
+                        if($scope.needs==undefined){
+                            $scope.needs=false;                            
+                        }
                  var dataToPost = {
-                        username: $scope.user,
-                        password1: $scope.password1,
-                        password2: $scope.password2,
-                        email:$scope.email,
+                        
                         first_name:$scope.name,
                         last_name:$scope.lastname,
-                        mobile_phone:$scope.phone
+                        email:$scope.email,
+                        mobile_phone:$scope.phone,                        
+                        offers_parking: $scope.offers,
+                        needs_parking: $scope.needs
                 }; 
                 
                 var config = {
                     headers:  {
+                        'Content-Type': 'application/json',
                         
-                        'Content-Type': 'application/json'
                     }
                 };
-                $http.post("http://192.241.211.179:8000/rest-auth/registration/" , dataToPost, config)
+                
+                
+                 
+                $http.post("http://192.241.211.179:8000/users/" , dataToPost, config)
                         .success(function(serverResponse, status, headers, config) {
                            $scope.Success = "Gracias por registrarte, te enviaremos más información a tu correo.";
+						   $scope.Error = "";
                         }).error(function(serverResponse, status, headers, config) {
-                            $scope.Error = "Error al agregar Ususario";
+                            $scope.Success = "";
+						   $scope.Error = "No se pudo registrar, verifique su información";
                         }
                     );
                 
